2025ce409f23a01cbbe9476d22f56f5eb87028c2
UniformSoup <halad@outlook.com>
2024-12-03 02:52:45 +0000

    Updating Adore...

--- main.cpp
+++ main.cpp
@@ -1,7 +1,16 @@
 #include <iostream>
+#include <Adore/Adore.hpp>
+#include <thread>
 
 int main()
 {
-    std::cout << "Hello, World!\n";
+    // ADORE_LOG(INFO, "This is INFO!!!");
+    // ADORE_LOG(WARN, "This is a warning!!!");
+    // ADORE_LOG(ERROR, "This is an error!!!");
+    // Adore::AdoreException("This is an exception");
+
+    auto ctx = Adore::Context::create(Adore::API::Vulkan, "UTT");
+    auto window = Adore::Window::create(ctx, "UTT Window");
+
     return EXIT_SUCCESS; 
 }
