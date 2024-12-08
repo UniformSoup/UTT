c62f35317a2cd517f7ab1095082f668947480f05
UniformSoup <halad@outlook.com>
2024-12-08 14:54:04 +0000

    triangle :)

--- main.cpp
+++ main.cpp
@@ -9,8 +9,28 @@ int main()
     // ADORE_LOG(ERROR, "This is an error!!!");
     // Adore::AdoreException("This is an exception");
 
-    auto ctx = Adore::Context::create(Adore::API::Vulkan, "UTT");
-    auto window = Adore::Window::create(ctx, "UTT Window");
+    try
+    {
+        auto ctx = Adore::Context::create(Adore::API::Vulkan, "UTT");
+        auto window = Adore::Window::create(ctx, "UTT Window");
+        auto renderer = Adore::Renderer::create(window);
+        auto shader = Adore::Shader::create(renderer, 
+        {
+            {Adore::Shader::Type::VERTEX, "Shaders/triangle.vert.spv"},
+            {Adore::Shader::Type::FRAGMENT, "Shaders/triangle.frag.spv"}
+        });
+
+        while (window->is_open())
+        {
+            window->poll();
+            renderer->render(shader);
+        }
+    }
+    catch (std::exception const& e)
+    {
+        std::cerr << e.what() << std::endl;
+        return EXIT_FAILURE;
+    }
 
     return EXIT_SUCCESS; 
 }
