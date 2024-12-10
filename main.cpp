0678e65e4546e2a04380724cd69766befe88c34e
UniformSoup <halad@outlook.com>
2024-12-10 19:50:55 +0000

    Uniforms.

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 #include <Adore/Adore.hpp>
 #include <thread>
 #include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
 
 #include <chrono>
 
@@ -11,22 +12,27 @@ struct Vertex
     glm::vec3 color;
 };
 
+struct Uniform
+{
+    glm::mat4 model, view, proj;
+};
+
 int main()
 {
-    // ADORE_LOG(INFO, "This is INFO!!!");
-    // ADORE_LOG(WARN, "This is a warning!!!");
-    // ADORE_LOG(ERROR, "This is an error!!!");
-    // Adore::AdoreException("This is an exception");
-
     std::vector<Vertex> vertices =
     {
-        {{-0.5f, -0.5f}, {1.0f, 0.0f, 0.0f}},
-        {{0.5f, -0.5f}, {0.0f, 1.0f, 0.0f}},
-        {{0.5f, 0.5f}, {0.0f, 0.0f, 1.0f}},
+        {{-0.5f, -0.5f}, {1.0f, 1.0f, 0.0f}},
+        {{0.5f, -0.5f}, {1.0f, 0.0f, 1.0f}},
+        {{0.5f, 0.5f}, {0.0f, 1.0f, 1.0f}},
         {{-0.5f, 0.5f}, {1.0f, 0.0f, 1.0f}}
     };
 
     std::vector<uint16_t> indices = {0, 1, 2, 2, 3, 0};
+
+    Uniform uniform;
+    uniform.model = glm::mat4(1.0f);
+    uniform.view = glm::mat4(1.0f);
+    uniform.proj = glm::mat4(1.0f);
 
     try
     {
@@ -35,33 +41,38 @@ int main()
         auto renderer = Adore::Renderer::create(window);
         auto buffer = Adore::VertexBuffer::create(renderer, (void*) vertices.data(), sizeof(Vertex) * vertices.size());
         auto indexbuffer = Adore::IndexBuffer::create(renderer, (void*) indices.data(), sizeof(uint16_t) * indices.size());
+        auto ubo = Adore::UniformBuffer::create(renderer, (void*) &uniform, sizeof(Uniform));
         auto shader = Adore::Shader::create(window, 
             {
-                {Adore::Shader::Type::VERTEX, "Shaders/triangle.vert.spv"},
-                {Adore::Shader::Type::FRAGMENT, "Shaders/triangle.frag.spv"}
+                {Adore::ShaderType::VERTEX, "Shaders/triangle.vert.spv"},
+                {Adore::ShaderType::FRAGMENT, "Shaders/triangle.frag.spv"}
             },
             {
                 {
-                    {0, 0, offsetof(Vertex, pos), Adore::Shader::InputDescriptor::Format::VEC2_FLOAT},
-                    {0, 1, offsetof(Vertex, color), Adore::Shader::InputDescriptor::Format::VEC3_FLOAT}
+                    {0, 0, offsetof(Vertex, pos), Adore::AttributeFormat::VEC2_FLOAT},
+                    {0, 1, offsetof(Vertex, color), Adore::AttributeFormat::VEC3_FLOAT}
                 },
                 {
                     {0, sizeof(Vertex)}
+                },
+                {
+                    {0, 1, Adore::ShaderType::VERTEX, Adore::UniformType::BUFFER}
                 }
+            },
+            {
+                ubo
             });
 
-        auto t1 = std::chrono::steady_clock::now();
+        auto clock = std::chrono::steady_clock::now();
+
         while (window->is_open())
         {
+            auto dt = (float) std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::steady_clock::now() - clock).count();
+            clock = std::chrono::steady_clock::now();
             window->poll();
-            if (std::chrono::steady_clock::now() - t1 > std::chrono::milliseconds(50))
-            {
-                for (auto& v : vertices)
-                    v.color = glm::vec3(v.color.z, v.color.x, v.color.y);
 
-                buffer = Adore::VertexBuffer::create(renderer, (void*) vertices.data(), sizeof(Vertex) * vertices.size());
-                t1 = std::chrono::steady_clock::now();
-            }
+            uniform.model = glm::rotate(uniform.model, 0.0004f * dt, glm::vec3(0.0f, 0.0f, 1.0f));
+            ubo->set(&uniform);
 
             renderer->begin(shader);
                 renderer->bind(buffer, 0);
